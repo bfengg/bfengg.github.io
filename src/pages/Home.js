3963152049c96495556b9b1522a5f24e1db4d6d4
brian <brian@antimetal.com>
2025-05-18 12:33:04 -0400

    cursor update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,38 @@
 import { CiLinkedin } from "react-icons/ci";
 import { LuGithub } from "react-icons/lu";
 import { AiOutlineMediumWorkmark } from "react-icons/ai";
+import { useState } from "react";
 import "../styles/Home.css";
 
-
 function Home() {
-        return (
-            <div className="home">
-                <div className="about">               
-                    <h2> Hey, this is Brian.</h2>
-                    <div className="subtitle">
-                        <p>I <i>build</i>, <i>read</i>, and <i>golf</i>.</p>
-                        <a href="https://github.com/bfengg"><LuGithub /></a>
-                        <a href="https://medium.com/@brianfeng717_79832"><AiOutlineMediumWorkmark /></a>
-                        <a href="https://www.linkedin.com/in/briantfeng/"><CiLinkedin /></a>
-                        
-                    </div>
-                </div>
-            </div>
-        );
-      }
+  const [isGolfMode, setIsGolfMode] = useState(false);
+
+  return (
+    <div className={`home ${isGolfMode ? "golf-mode" : ""}`}>
+      <div className="about">
+        <h2> Hey, this is Brian.</h2>
+        <div className="subtitle">
+          <p>
+            I <i>build</i>, <i>read</i>, and{" "}
+            <i>
+              <span className="golf-text" onClick={() => setIsGolfMode(true)}>
+                golf
+              </span>
+            </i>
+            .
+          </p>
+          <a href="https://github.com/bfengg">
+            <LuGithub />
+          </a>
+          <a href="https://medium.com/@brianfeng717_79832">
+            <AiOutlineMediumWorkmark />
+          </a>
+          <a href="https://www.linkedin.com/in/briantfeng/">
+            <CiLinkedin />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default Home;
